8da49db21c48af33fe638f9ad97ca42e23ad6218
netanmangal <imnetanmangal@gmail.com>
2022-02-17 22:56:31 +0530

    Update: deploy.js

--- smart-contract/deploy.js
+++ smart-contract/deploy.js
@@ -2,7 +2,7 @@ const Web3 = require("web3");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 require("dotenv").config();
 
-const {abi, evm} = require("./build/"); // .json file path
+const {abi, evm} = require("./build/ERC20Key.json"); // .json file path
 
 const provider = new HDWalletProvider(
     process.env.MNEMONIC,
@@ -16,9 +16,11 @@ const deploy = async() => {
     console.log(accounts);
 
     const contract = await new web3.eth.Contract(abi)
-        .deploy({data: evm.bytecode.object, arguments: []})  // add arguments to constructor inside array
-        .send({from: accounts[0], gas: "1000000"});
+        .deploy({data: evm.bytecode.object, arguments: ["NetizaqToken", "NTQ"]})  // add arguments to constructor inside array
+        .send({from: accounts[0], gas: "4000000"});
     console.log(contract.options.address);
+
+    console.log(`name of our token is: ${await contract.methods.name().call()}`);
 }
 
 deploy();
