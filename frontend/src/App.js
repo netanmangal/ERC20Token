513e4b4465809b4083490f7b98c0f5b395d93630
netanmangal <imnetanmangal@gmail.com>
2022-02-18 22:34:18 +0530

    Update: UI + web3 features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
+import { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import web3 from "./web3.js";
+import contract from "./contract.js";
 
 function App() {
+  let [tokenName, setTokenName] = useState();
+  let [tokenSymbol, setTokenSymbol] = useState();
+  let [accounts, setAccounts] = useState([]);
+
+  useEffect(async () => {
+    setAccounts( await web3.eth.getAccounts() );
+    setTokenName( await contract.methods.name().call() );
+    setTokenSymbol( await contract.methods.symbol().call() );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>You are connected via address: <b>{accounts[0]}</b></p>
+        <h2>ERC-20 Token Details:</h2>
+        <p>Name: {tokenName}</p>
+        <p>Symbol: {tokenSymbol}</p>
       </header>
     </div>
   );
